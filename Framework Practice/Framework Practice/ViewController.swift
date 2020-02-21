99978875f65c3343094007c03097285862c2640c
Isha Vason <isha.vason@tothenew.com>
2020-02-21 16:35:00 +0530

    Initial Setup

---
+++ Framework Practice/Framework Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Framework Practice
+//
+//  Created by Ashish Singh on 21/02/20.
+//  Copyright © 2020 Ashish Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
